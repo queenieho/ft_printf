fbd81bd9899b8f31e3b9bdf3f389259aff7a8ee4
Queenie Ho <kuini.ho@gmail.com>
2017-04-19 23:30:19 -0700

    cleaning memory leaks

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: qho <qho@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/03/08 13:42:02 by qho               #+#    #+#              #
-#    Updated: 2017/04/18 14:45:43 by qho              ###   ########.fr        #
+#    Updated: 2017/04/19 22:00:47 by qho              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,7 +22,7 @@ INC		=	./inc/
 FILES	=	ft_getdata ft_make_num ft_make_ptr ft_make_str ft_make_wstr 	\
 			ft_make_wstr_2		\
 			ft_modnum ft_modnum_2 ft_parse ft_parse_2 ft_printf ft_put_pf	\
-			ft_strings_1 ft_strings_2 ft_width_pres ft_wstrings_1
+			ft_strings_1 ft_strings_2 ft_width_pres ft_wstrings_1 random
 SRC		=	$(addprefix src/, $(addsuffix .c, $(FILES)))
 
 # Object directory and files
@@ -42,7 +42,9 @@ $(OBJ):	$(SRC)
 	@echo "\033[32mObject files made\033[0m"
 
 test: $(NAME)
-	@$(CC) -Wformat -I$(INC) $(NAME) main.c -o $(EXE)
+	# @$(CC) -Wformat -I$(INC) $(NAME) main.c -o $(EXE)
+	# clang -fsanitize=address -I$(INC) $(NAME) main.c
+	clang -I$(INC) $(NAME) main.c
 
 norm:
 	@norminette $(SRC) $(INC)
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: qho <qho@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 10:56:39 by qho               #+#    #+#             */
-/*   Updated: 2017/04/18 19:03:47 by qho              ###   ########.fr       */
+/*   Updated: 2017/04/19 23:18:31 by qho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 void	ft_print_char(t_flags *flag, t_data *data, t_format *str)
 {
+	char	*print;
+
+	print = NULL;
 	if (flag->conv_i == 9 && flag->lm == 0)
 	{
 		if (data->c == '\0' && flag->minus)
 			ft_putchar_pf('\0', &str->cnt);
-		ft_putstr_pf(ft_make_c(flag, data->c, &str->cnt), &str->cnt);
+		print = ft_make_c(flag, data->c, &str->cnt);
+		ft_putstr_pf(print, &str->cnt);
 		if (data->c == '\0' && !flag->minus)
 			ft_putchar_pf('\0', &str->cnt);
 	}
@@ -31,24 +35,33 @@ void	ft_print_char(t_flags *flag, t_data *data, t_format *str)
 		if (data->w_chr == '\0' && !flag->minus)
 			ft_putwchar_pf('\0', &str->cnt);
 	}
+	if (print)
+		free(print);
 }
 
 void	ft_print_conv(t_flags *flag, t_data *data, t_format *str)
 {
+	char	*print;
+
+	print = NULL;
 	if (flag->conv_i >= 0 && flag->conv_i <= 8)
-		ft_putstr_pf(ft_make_num(flag, data->nb), &str->cnt);
+		print = ft_make_num(flag, data->nb);
 	else if (flag->conv_i == 9 || flag->conv_i == 10)
 		ft_print_char(flag, data, str);
 	else if (flag->conv_i == 11 && flag->lm == 0)
-		ft_putstr_pf(ft_make_s(flag, data->str), &str->cnt);
+		print = ft_make_s(flag, data->str);
 	else if ((flag->conv_i == 12) || (flag->conv_i == 11 && flag->lm == 3))
 		ft_putwstr_pf(ft_make_wstr(flag, data->w_str), &str->cnt);
 	else if (flag->conv_i == 13)
-		ft_putstr_pf(ft_make_ptr(flag, data->ptr), &str->cnt);
+		print = ft_make_ptr(flag, data->ptr);
 	else if (flag->conv_i == 14)
-		ft_putstr_pf(ft_make_c(flag, data->c, &str->cnt), &str->cnt);
+		print = ft_make_c(flag, data->c, &str->cnt);
 	else if (flag->conv_i == -1 && flag->conv != '\0')
-		ft_putstr_pf(ft_make_c(flag, flag->conv, &str->cnt), &str->cnt);
+		print = ft_make_c(flag, flag->conv, &str->cnt);
+
+	ft_putstr_pf(print, &str->cnt);
+	if (print)
+		free(print);
 }
 
 int		ft_invalid(char *str)
@@ -125,3 +138,34 @@ int		ft_printf(const char *restrict format, ...)
 	va_end(arg);
 	return (str.cnt);
 }
+
+// int		ft_printf(const char *restrict format, ...)
+// {
+// 	va_list		arg;
+// 	t_flags		*flag;
+// 	t_data		*data;
+// 	t_format	*str;
+
+// 	va_start(arg, format);
+// 	flag = (t_flags*)malloc(sizeof(t_flags));
+// 	data = (t_data*)malloc(sizeof(t_data));
+// 	str = (t_format*)malloc(sizeof(t_format));
+// 	ft_bzero(str, sizeof(t_format));
+// 	ft_bzero(flag, sizeof(t_flags));
+// 	ft_bzero(data, sizeof(t_data));
+// 	str->tmp = (char *)format;
+// 	while (*str->tmp)
+// 	{
+// 		if (*str->tmp != '%')
+// 			ft_putchar_pf(*str->tmp, &str->cnt);
+// 		else
+// 		{
+// 			ft_conversion(&arg, flag, data, str);
+// 			ft_bzero(flag, sizeof(t_flags));
+// 			ft_bzero(data, sizeof(t_data));
+// 		}
+// 		str->tmp++;
+// 	}
+// 	va_end(arg);
+// 	return (str->cnt);
+// }
